992d915882e363493342c1734744562af80f4577
Alexsander <alex.moura@soudivergente.com.br>
2022-07-12 21:02:12 -0300

    Adicionando tela

--- tinder/lib/main.dart
+++ tinder/lib/main.dart
@@ -1,115 +1,180 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyTinder());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyTinder extends StatelessWidget {
+  const MyTinder({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: "My Tinder",
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: <Color>[
+            Color(0xFFED7361),
+            Color(0xFFE94976),
+          ])),
+      child: Scaffold(
+        backgroundColor: const Color.fromRGBO(0, 0, 0, 0.0),
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(0, 0, 0, 0.0),
+          shadowColor: const Color.fromRGBO(0, 0, 0, 0.0),
+          leading: const Icon(
+            Icons.chevron_left_rounded,
+            size: 35,
+          ),
         ),
+        body: const ContentHome(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ContentHome extends StatelessWidget {
+  const ContentHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size.width;
+
+    return SizedBox(
+      width: size,
+      child: Column(
+        children: [
+          const SizedBox(height: 150),
+          SizedBox(
+            width: size * 0.6,
+            child: Image.asset('assets/images/Frame.png'),
+          ),
+          const SizedBox(
+            height: 90,
+          ),
+          SizedBox(
+            width: size * 0.8,
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                  text: 'By tapping Create Accountor Sign In you agree to our ',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Terms',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold)),
+                    TextSpan(text: '. Learn how we process your datain our '),
+                    TextSpan(
+                        text: 'Privacy Policy',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold)),
+                    TextSpan(text: ' and '),
+                    TextSpan(
+                        text: 'Cookies Policy',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold)),
+                    TextSpan(text: '.'),
+                  ]),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ButtomComponent(
+            size: size * 0.9,
+            icon: const Icon(
+              Icons.apple,
+              color: Colors.white,
+              size: 30,
+            ),
+            text: const Text(
+              'SIGN IN WITH APPLE',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 18),
+          ButtomComponent(
+            size: size * 0.9,
+            icon: const Icon(
+              Icons.facebook,
+              color: Colors.white,
+              size: 30,
+            ),
+            text: const Text(
+              'SIGN IN WITH FACEBOOK',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 18),
+          ButtomComponent(
+            size: size * 0.9,
+            icon: const Icon(
+              Icons.messenger_rounded,
+              color: Colors.white,
+              size: 30,
+            ),
+            text: const Text(
+              'SIGN IN WITH PHONE NUMBER',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            'Trouble Signing in?',
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ButtomComponent extends StatelessWidget {
+  final double size;
+  final Icon icon;
+  final Text text;
+  const ButtomComponent(
+      {Key? key, required this.size, required this.icon, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(
+            width: 2.0, color: Colors.white, style: BorderStyle.solid),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        children: [
+          SizedBox(width: 60, child: icon),
+          Expanded(child: text),
+        ],
+      ),
     );
   }
 }
